6e2e97d0bd1223b7e9e381c657bc509cfab5f64f
kingsley <ayoguchimezie00@gmail.com>
2025-07-10 14:04:59 +0000

    confused herwe

--- about/index.php
+++ about/index.php
@@ -21,8 +21,7 @@
 
 <body class="with-sidebar">
     <div class="loader-wrapper">
-        <div class="loader">
-        </div>
+        <div class="loader"></div>
         <div class="loader-section section-left"></div>
         <div class="loader-section section-right"></div>
     </div>
